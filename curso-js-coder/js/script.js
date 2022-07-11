590eafae7ff1cd5d750a4dba84ccac25ddd3e6f4
nataligala <natali.rabuffetti@gmail.com>
2022-07-11 01:35:52 -0300

    Desafio complementario: Crear un algoritmo utilizando un ciclo

---
+++ curso-js-coder/js/script.js
@@ -0,0 +1,32 @@
+//Pedir número mediante prompt y sumarle otro número en cada repetición, realizando una salida por cada resultado
+
+// let pedirNumero = parseInt(prompt("Por favor, ingrese un numero"));
+// let resultado = 0;
+
+// for( let i = 1; i < 10; i++ ){
+//     resultado = pedirNumero + i;
+//     console.log(resultado);
+// }
+
+
+
+//Pedir un texto mediante prompt, concatenar un valor en cada repetición, realizando una salida por cada resultado, hasta que se ingrese "ESC"
+
+// let pedirTexto ='';
+
+// do {
+//     pedirTexto = prompt('Por favor, ingrese su nombre. Si desea terminar ingrese "ESC"');
+//     alert('Bienvenido ' + pedirTexto);
+// }while(pedirTexto != "ESC")
+
+
+
+// //Pedir un número por prompt, repetir la salida del mensaje "Hola" la cantidad de veces ingresada.
+
+// let pedirNumero = parseInt(prompt("Por favor, ingrese un numero"));
+
+// for(let i=1; i<= pedirNumero; i++){
+//     alert("Hola");
+// }
+
+
